9967d7f95f51f95501faa46eb0a27d41ef02121e
lazdinst <lazdinst@gmail.com>
2025-03-29 07:42:13 -0400

    restructure

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import { ThemeProvider } from './styled';
+import store from './redux/store/index.ts';
+import { ThemeProvider } from './styled/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
